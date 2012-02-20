1056a7fbbe2d1cf71f12af01443092c978c89b13
Mikael Gramont <mgramont@gmail.com>
2012-02-20 09:01:18 -0800

    Added instructions and setup script

---
+++ bin/setup.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/bash
+
+mkdir ../data/backups
+chmod 777 ../data/backups
+
+mkdir ../data/cache
+
+mkdir ../data/cache/api
+chmod 777 ../data/cache/api
+
+mkdir ../data/cache/test
+chmod 777 ../data/cache/test
+
+mkdir ../logs
+
+mkdir ../logs/api
+chmod 777 ../logs/api
+
+mkdir ../logs/test
+chmod 777 ../logs/test
+
+mkdir ../data/rawUploads
+chmod 777 ../data/rawUploads
+
+mkdir ../data/sessions
+chmod 777 ../data/sessions
+
+mkdir ../public/media
+chmod 777 ../public/media
+
+mkdir ../public/media/thumbnails
+chmod 777 ../public/media/thumbnails
