fe43ffe3585ff5334db2fb0f928efc0bc228259e
Mikael Gramont <mgramont@gmail.com>
2019-01-19 12:01:12 +0100

    Only JSON-decode the request body when requests are made with the application/json content-type.

--- library/Lib/Controller/Plugin/RequestHandler.php
+++ library/Lib/Controller/Plugin/RequestHandler.php
@@ -2,13 +2,16 @@
 
 class Lib_Controller_Plugin_RequestHandler extends Zend_Controller_Plugin_Abstract
 {
+  const APPLICATION_JSON = 'application/json';
+
   public function preDispatch($request)
   {
     if (!$request instanceof Zend_Controller_Request_Http) {
       return;
     }
 
-    if ($request->isPut() || $request->isPost()) {
+    $contentType = $request->getHeader('Content-Type');
+    if ($contentType == self::APPLICATION_JSON && ($request->isPut() || $request->isPost())) {
       $jsonParams = Zend_Json::decode($request->getRawBody());
       $request->setParams($jsonParams);
     };
