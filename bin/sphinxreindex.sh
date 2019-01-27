680b54eab6dfc96769ae670668065cccbf1bec58
Mikael Gramont <mgramont@gmail.com>
2019-01-27 17:22:16 +0100

    Removed /bin folder

--- bin/sphinxreindex.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-/usr/local/bin/indexer --config /var/www/www.mountainboard.fr/current/application/configs/sphinx.conf --all --rotate
--- bin/sphinxstart.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-searchd -c /var/www/www.mountainboard.fr/current/application/configs/sphinx.conf
