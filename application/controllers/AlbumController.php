4dbf2b6f32cc16c8b8b2fa6ff34cc56afc9adacc
Mikael Gramont <mgramont@gmail.com>
2019-05-22 07:57:18 +0200

    All tests pass.

--- application/controllers/AlbumController.php
+++ application/controllers/AlbumController.php
@@ -13,6 +13,37 @@ class AlbumController extends Api_Controller_Action
     $object->setNotification(true);
   }
 
+  public function deleteAction()
+  {
+    $id = $this->_request->getParam('id');
+    if (!$id) {
+      throw new Api_Exception_BadRequest();
+    }
+    $result = $this->_table->find($id);
+    if (empty($result) || !$object = $result->current()) {
+      throw new Api_Exception_NotFound();
+    }
+
+    if ($object->albumType != Media_Album::TYPE_SIMPLE) {
+      throw new Api_Exception_BadRequest('Album not deletable', Api_ErrorCodes::NON_STATIC_ALBUM_CANNOT_BE_DELETED);
+    }
+
+    $itemSet = $object->getItemSet();
+    if (!$itemSet || count($itemSet) > 0) {
+      throw new Api_Exception_BadRequest('Album not empty', Api_ErrorCodes::STATIC_ALBUM_NOT_EMPTY);
+    }
+
+    if (!$object->isDeletableBy($this->_user, $this->_acl)) {
+      throw new Api_Exception_Unauthorised();
+    }
+
+    $this->_preObjectDelete($object);
+    if ($status = $this->_accessor->deleteObject($object)) {
+      $this->_postObjectDelete($object);
+    }
+    $this->view->output = array('status' => $status);
+  }
+
   protected function _getWhereClause(User_Row $user)
   {
     if (in_array($user->status, array(User::STATUS_EDITOR, User::STATUS_ADMIN))) {
