123b065b07a99fda2e8aacbd0ef4190c20e04aa6
Mikael Gramont <mgramont@gmail.com>
2020-02-20 15:54:52 +0100

    Some more utf8mb4 shenanigans

--- data/sql/migrations/013-database-as-utf8mb4.sql
+++ data/sql/migrations/013-database-as-utf8mb4.sql
@@ -1,3 +1,5 @@
 ALTER DATABASE `surfingdirtcom_beta` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER DATABASE `surfingdirtcom_dev` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER DATABASE `surfingdirtcom_prod` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+ALTER DATABASE `ridedb_test` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- data/sql/structure.sql
+++ data/sql/structure.sql
@@ -1,216 +1,234 @@
-/*!40101 SET NAMES utf8mb4 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+# Dump of table comments
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `comments`;
+
 CREATE TABLE `comments` (
-  `id` varchar(36) NOT NULL,
-  `parentId` varchar(36)  NOT NULL,
-  `parentType` varchar(64) NOT NULL,
-  `content` TEXT COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `date` datetime NOT NULL,
-  `submitter` varchar(36) NOT NULL,
-  `lastEditionDate` datetime DEFAULT NULL,
-  `lastEditor` varchar(36) DEFAULT NULL,
-  `status` enum('valid','invalid') NOT NULL DEFAULT 'invalid',
-  `tone` varchar(32) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+    `id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `parentId` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `parentType` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `content` text COLLATE utf8mb4_unicode_ci,
+    `date` datetime NOT NULL,
+    `submitter` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `lastEditionDate` datetime DEFAULT NULL,
+    `lastEditor` varchar(36) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `status` enum('valid','invalid') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'invalid',
+    `tone` varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-/*Table structure for table `images` */
+
+
+# Dump of table images
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `images`;
 
 CREATE TABLE `images` (
-  `id` varchar(36) NOT NULL,
-  `imageType` int(11) NOT NULL,
-  `storageType` int(11) NOT NULL,
-  `status` enum('valid','invalid') NOT NULL DEFAULT 'valid',
-  `submitter` varchar(36) NOT NULL,
-  `date` datetime(3) NOT NULL,
-  `lastEditionDate` datetime(3) DEFAULT NULL,
-  `lastEditor` varchar(36) DEFAULT NULL,
-  `width` int(11) NOT NULL,
-  `height` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+    `id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `imageType` int(11) NOT NULL,
+    `storageType` int(11) NOT NULL,
+    `status` enum('valid','invalid') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'valid',
+    `submitter` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `date` datetime(3) NOT NULL,
+    `lastEditionDate` datetime(3) DEFAULT NULL,
+    `lastEditor` varchar(36) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `width` int(11) NOT NULL,
+    `height` int(11) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-/*Table structure for table `item_views` */
+
+
+# Dump of table item_views
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `item_views`;
 
 CREATE TABLE `item_views` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `itemId` int(11) NOT NULL,
-  `itemType` varchar(40) NOT NULL,
-  `views` int(11) NOT NULL DEFAULT '0',
-  `lastView` datetime(3) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `item` (`itemId`,`itemType`),
-  KEY `date` (`lastView`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `itemId` int(11) NOT NULL,
+    `itemType` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `views` int(11) NOT NULL DEFAULT '0',
+    `lastView` datetime(3) DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `item` (`itemId`,`itemType`),
+    KEY `date` (`lastView`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-/*Table structure for table `items` */
+
+
+# Dump of table items
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `items`;
 
 CREATE TABLE `items` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Index of the item in its table',
-  `itemId` varchar(36) NOT NULL DEFAULT '0',
-  `itemType` varchar(32) NOT NULL DEFAULT '' COMMENT 'Type of the item (equivalent to its table)',
-  `date` datetime(3) DEFAULT NULL,
-  `status` enum('valid','invalid') NOT NULL DEFAULT 'invalid',
-  `parentItemId` int(11) unsigned DEFAULT NULL,
-  `submitter` varchar(36) NOT NULL,
-  `notification` enum('announce','silent') DEFAULT 'announce',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `item` (`date`,`itemId`,`itemType`) USING BTREE,
-  KEY `parentItemId` (`parentItemId`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+    `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Index of the item in its table',
+    `itemId` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '0',
+    `itemType` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'Type of the item (equivalent to its table)',
+    `date` datetime(3) DEFAULT NULL,
+    `status` enum('valid','invalid') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'invalid',
+    `parentItemId` int(11) unsigned DEFAULT NULL,
+    `submitter` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `notification` enum('announce','silent') COLLATE utf8mb4_unicode_ci DEFAULT 'announce',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `item` (`date`,`itemId`,`itemType`) USING BTREE,
+    KEY `parentItemId` (`parentItemId`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+
+# Dump of table media_album_aggregations
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `media_album_aggregations`;
 
 CREATE TABLE `media_album_aggregations` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'albumId',
-  `keyName` varchar(45) CHARACTER SET utf8 NOT NULL,
-  `keyValue` varchar(36) NOT NULL,
-  `albumId` varchar(36) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `Index_2` (`albumId`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+    `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'albumId',
+    `keyName` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `keyValue` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `albumId` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '0',
+    PRIMARY KEY (`id`),
+    KEY `Index_2` (`albumId`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-/*Table structure for table `media_albums` */
+
+
+# Dump of table media_albums
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `media_albums`;
 
 CREATE TABLE `media_albums` (
-  `id` varchar(36) NOT NULL,
-  `date` datetime(3) NOT NULL,
-  `submitter` varchar(36) NOT NULL,
-  `lastEditionDate` datetime(3) DEFAULT NULL,
-  `lastEditor` varchar(36) DEFAULT NULL,
-  `title` TEXT COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `description` TEXT COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `status` enum('valid','invalid') NOT NULL DEFAULT 'invalid',
-  `albumType` enum('simple','aggregate') NOT NULL DEFAULT 'simple' COMMENT 'Simple or aggregated',
-  `albumContributions` enum('private','public') NOT NULL DEFAULT 'private',
-  `albumCreation` enum('static','automatic','user') NOT NULL DEFAULT 'automatic',
-  `albumVisibility` enum('private','unlisted','visible') NOT NULL DEFAULT 'visible',
-  PRIMARY KEY (`id`)
+      `id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+      `date` datetime(3) NOT NULL,
+      `submitter` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+      `lastEditionDate` datetime(3) DEFAULT NULL,
+      `lastEditor` varchar(36) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+      `title` text COLLATE utf8mb4_unicode_ci,
+      `description` text COLLATE utf8mb4_unicode_ci,
+      `status` enum('valid','invalid') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'invalid',
+      `albumType` enum('simple','aggregate') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'simple' COMMENT 'Simple or aggregated',
+      `albumContributions` enum('private','public') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'private',
+      `albumCreation` enum('static','automatic','user') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'automatic',
+      `albumVisibility` enum('private','unlisted','visible') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'visible',
+      PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-/*Table structure for table `media_albums_items` */
+
+
+# Dump of table media_albums_items
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `media_albums_items`;
 
 CREATE TABLE `media_albums_items` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `itemType` varchar(32) NOT NULL,
-  `itemId` varchar(36) NOT NULL,
-  `albumId` varchar(36) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `item` (`itemType`,`itemId`),
-  UNIQUE KEY `album` (`albumId`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `itemType` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `itemId` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `albumId` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `item` (`itemType`,`itemId`),
+    UNIQUE KEY `album` (`albumId`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-/*Table structure for table `media_items` */
+
+
+# Dump of table media_items
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `media_items`;
 
 CREATE TABLE `media_items` (
-  `id` varchar(36) NOT NULL,
-  `mediaType` varchar(255) DEFAULT NULL,
-  `mediaSubType` varchar(255) NOT NULL,
-  `vendorKey` varchar(1024) DEFAULT NULL,
-  `albumId` varchar(36) NOT NULL,
-  `title` TEXT COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `description` TEXT COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `storageType` varchar(255) NOT NULL,
-  `imageId` varchar(36) NOT NULL,
-  `width` int(10) unsigned NOT NULL,
-  `height` int(10) unsigned NOT NULL,
-  `status` enum('valid','invalid') NOT NULL DEFAULT 'invalid',
-  `submitter` varchar(36) NOT NULL,
-  `date` datetime(3) NOT NULL,
-  `lastEditionDate` datetime(3) DEFAULT NULL,
-  `lastEditor` varchar(36) DEFAULT NULL,
-  UNIQUE KEY `imageId` (`imageId`),
-  PRIMARY KEY (`id`)
+     `id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+     `mediaType` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+     `mediaSubType` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+     `vendorKey` varchar(1024) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+     `albumId` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+     `title` text COLLATE utf8mb4_unicode_ci,
+     `description` text COLLATE utf8mb4_unicode_ci,
+     `storageType` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+     `imageId` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+     `width` int(10) unsigned NOT NULL,
+     `height` int(10) unsigned NOT NULL,
+     `status` enum('valid','invalid') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'invalid',
+     `submitter` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+     `date` datetime(3) NOT NULL,
+     `lastEditionDate` datetime(3) DEFAULT NULL,
+     `lastEditor` varchar(36) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+     PRIMARY KEY (`id`),
+     UNIQUE KEY `imageId` (`imageId`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-/*Table structure for table `media_items_users` */
+
+
+# Dump of table media_items_users
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `media_items_users`;
 
 CREATE TABLE `media_items_users` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `mediaId` varchar(36) NOT NULL,
-  `userId` varchar(36) NOT NULL,
-  `userName` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
-
-/*Table structure for table `translated_texts` */
-
-DROP TABLE IF EXISTS `translated_texts`;
-
-CREATE TABLE `translated_texts` (
-  `id` varchar(36) NOT NULL,
-  `itemType` varchar(64) CHARACTER SET utf8 NOT NULL,
-  `lang` char(2) CHARACTER SET utf8 NOT NULL,
-  `type` enum('title','description','content') CHARACTER SET utf8 NOT NULL,
-  `text` text CHARACTER SET utf8 NOT NULL,
-  PRIMARY KEY (`id`,`itemType`,`lang`,`type`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
-
-/*Table structure for table `user_notifications` */
-
-DROP TABLE IF EXISTS `user_notifications`;
-
-CREATE TABLE `user_notifications` (
-  `userId` varchar(36) NOT NULL,
-  `itemType` varchar(30) NOT NULL,
-  `medium` enum('none','homePage','email','twitter','facebook') NOT NULL DEFAULT 'none',
-  `notify` tinyint(1) DEFAULT '0',
-  PRIMARY KEY (`userId`,`medium`,`itemType`) USING BTREE
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
-
-/*Table structure for table `users` */
-
-DROP TABLE IF EXISTS `users`;
-
-CREATE TABLE `users` (
-  `userId` varchar(36) NOT NULL,
-  `username` varchar(64) NOT NULL,
-  `password` varchar(128) DEFAULT NULL,
-  `salt` varchar(36) NOT NULL DEFAULT '',
-  `email` varchar(128) NOT NULL,
-  `status` enum('banned','guest','pending','member','writer','editor','admin') NOT NULL DEFAULT 'guest',
-  `date` datetime(3) DEFAULT NULL,
-  `lastLogin` datetime(3) DEFAULT NULL,
-  `locale` varchar(128) DEFAULT NULL,
-  `timezone` varchar(128) DEFAULT NULL,
-  `firstName` varchar(64) DEFAULT NULL,
-  `lastName` varchar(64) DEFAULT NULL,
-  `city` varchar(64) DEFAULT NULL,
-  `site` varchar(255) DEFAULT NULL,
-  `activationKey` varchar(32) DEFAULT NULL,
-  `newPassword` varchar(128) DEFAULT NULL,
-  `avatar` varchar(36) DEFAULT NULL,
-  `cover` varchar(36) DEFAULT NULL,
-  `bio` TEXT COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`userId`)
+    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `mediaId` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `userId` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `userName` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-/*Table structure for table `migrations` */
+
+
+# Dump of table migrations
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `migrations`;
 
 CREATE TABLE `migrations` (
-  `id` varchar(255) NOT NULL,
-  `date` datetime(3) NOT NULL
-);
+    `id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `date` datetime(3) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+
+
+# Dump of table user_notifications
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `user_notifications`;
+
+CREATE TABLE `user_notifications` (
+    `userId` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `itemType` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `medium` enum('none','homePage','email','twitter','facebook') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'none',
+    `notify` tinyint(1) DEFAULT '0',
+    PRIMARY KEY (`userId`,`medium`,`itemType`) USING BTREE
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+
+# Dump of table users
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+    `userId` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `username` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `password` varchar(128) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `salt` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+    `email` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `status` enum('banned','guest','pending','member','writer','editor','admin') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'guest',
+    `date` datetime(3) DEFAULT NULL,
+    `lastLogin` datetime(3) DEFAULT NULL,
+    `locale` varchar(128) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `timezone` varchar(128) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `firstName` varchar(64) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `lastName` varchar(64) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `city` varchar(64) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `site` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `activationKey` varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `newPassword` varchar(128) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `avatar` varchar(36) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `cover` varchar(36) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `bio` text COLLATE utf8mb4_unicode_ci,
+    PRIMARY KEY (`userId`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
