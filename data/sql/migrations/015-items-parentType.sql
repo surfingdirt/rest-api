856c04a60fd9764fa9003d7408345eff6e2dd868
Mikael Gramont <mgramont@gmail.com>
2020-03-25 22:15:06 +0100

    Whoops

--- data/sql/migrations/015-items-parentType.sql
+++ data/sql/migrations/015-items-parentType.sql
@@ -3,5 +3,4 @@ ALTER TABLE `items` ADD COLUMN `parentItemType` varchar(32) DEFAULT NULL after `
 UPDATE items as child
     INNER JOIN items as parent
     ON child.parentItemId = parent.itemId
-SET child.parentItemType = parent.itemType
-WHERE child.itemId = 'd155a85c-cecb-4432-803c-1307365a39d9';
+SET child.parentItemType = parent.itemType;
