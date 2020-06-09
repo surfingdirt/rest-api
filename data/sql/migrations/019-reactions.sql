4f4632588a60044802b6a15ad35bb28c50b9472a
Mikael Gramont <mgramont@gmail.com>
2020-06-09 09:16:19 +0200

    Update reactions migration file

--- data/sql/migrations/019-reactions.sql
+++ data/sql/migrations/019-reactions.sql
@@ -1,9 +1,13 @@
 CREATE TABLE `reactions` (
-    `userId` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
     `itemType` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
     `itemId` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
-    `date` datetime(3) NOT NULL,
     `type` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
-    PRIMARY KEY (`userId`),
-    UNIQUE KEY `item` (`userId`,`itemType`,`itemId`,`type`)
+    `status` enum('valid','invalid') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'invalid',
+    `submitter` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `date` datetime(3) NOT NULL,
+    `lastEditionDate` datetime(3) DEFAULT NULL,
+    `lastEditor` varchar(36) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `item` (`submitter`,`itemType`,`itemId`,`type`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
